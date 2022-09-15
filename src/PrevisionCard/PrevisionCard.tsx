c74143263d1285131c5e4f554a1de758ec59f8dd
Leonardo Ferreira Rochedo <learnir@outlook.com.br>
2022-09-15 10:00:06 -0300

    consulting API CountryFlags

--- src/PrevisionCard/PrevisionCard.tsx
+++ src/PrevisionCard/PrevisionCard.tsx
@@ -7,37 +7,55 @@ import './PrevisionCard.css'
 
 export function PrevisionCard() {
 
-    const [city, setCity] = useState({ name: '', temp: 0, description: '', humidity: '', wind: '' })
+    // Cidade que estamos buscando
+    const [city, setCity] = useState({ name: '', country: 'br', weatherIcon: '', temp: 0, description: '', humidity: '', wind: '' })
+    // Valor do input
+    const [cityName, setCityName] = useState('Londrina')
 
-    fetch(
-        "https://api.openweathermap.org/data/2.5/weather?q=Londrina&appid=f8620e3ccd0f4db1f3024ae2085e9600"
-      )
-        .then((response) => response.json())
-        .then((data) => {
-          setCity({
-            name: data.name,
-            description: data.weather[0].main,
-            temp: parseInt(data.main.temp),
-            humidity: data.main.humidity,
-            wind: data.wind.speed,
-          });
-        });
+    const APIKey = "f8620e3ccd0f4db1f3024ae2085e9600"
+
+    function consultAPI() {
+
+        // OpenWeather
+        fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKey}`
+          )
+            .then((response) => response.json())
+            .then((data) => {
+              setCity({
+                name: data.name,
+                country: data.sys.country,
+                weatherIcon: data.weather[0].icon,
+                description: data.weather[0].main,
+                temp: parseInt(data.main.temp),
+                humidity: data.main.humidity,
+                wind: data.wind.speed,
+              });
+            });
+
+    }
+
+    // CountryFlags
+    const countryFlag = `https://countryflagsapi.com/png/${city.country}`
+    // ícone da previsão
+    const weatherIconLink = `http://openweathermap.org/img/wn/${city.weatherIcon}`
+    
 
     return (
         <div className='container'>
             <div className="form">
                 <h3 className='title-form'>Consulte o clima de uma cidade:</h3>
                 <div className="form-input">
-                    <input type="text" placeholder='Digite o nome da cidade' />
-                    <button id='search'><FaSearch/></button>
+                    <input type="text" placeholder='Digite o nome da cidade' onChange={(e) => setCityName(e.target.value)} />
+                    <button id='search' onClick={consultAPI}><FaSearch/></button>
                 </div>
             </div>
 
             <div className="card-info">
-                <h2 className='city-name'><span id='pin'><FaMapPin/></span> {city.name} <img className='country' src="https://countryflagsapi.com/png/br" alt="Bandeira do país" /></h2>
+                <h2 className='city-name'><span id='pin'><FaMapPin/></span> {city.name} <img className='country' src={countryFlag} alt="Bandeira do país" /></h2>
                 <p className='city-temp'>{city.temp} &deg;C</p>
                 <div className="weather-description">
-                    <img className='temp' src="http://openweathermap.org/img/wn/01d@2x.png" alt="Condições do tempo" />
+                    <img className='temp' src={weatherIconLink} alt="Condições do tempo" />
                     <p className='city-description'>{city.description}</p>
                 </div>
                 <div className="weather-details">
